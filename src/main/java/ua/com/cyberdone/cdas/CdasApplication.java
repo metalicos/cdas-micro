b319188eb71adfd2c7db11505e97b96b31f374a0
Ostap Komplikevych <ostap.ja@gmail.com>
2021-12-26 15:36:00 +0200

    First commit

---
+++ src/main/java/ua/com/cyberdone/cdas/CdasApplication.java
@@ -0,0 +1,13 @@
+package ua.com.cyberdone.cdas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/com/cyberdone/cdas/CdasApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.cyberdone.cdas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
